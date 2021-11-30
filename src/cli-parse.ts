937fce98e225f64f2cd8bde37f9d7c51ede567eb
Jon Lauridsen <mail@jonlauridsen.com>
2021-11-30 09:28:36 +0100

    Move current CLI functionality into "lead-time" subcommand

--- src/cli-parse.ts
+++ src/cli-parse.ts
@@ -23,13 +23,21 @@ export function parseMainCli(program: Command, argv: string[]): CliArgs {
     .allowExcessArguments(false)
     .name("github-delivery-metrics")
     .description("This tool calculates various metrics that can be useful for discussions about delivery performance.")
+    .option("-d, --debug", "Enable debug logging");
+
+  program
+    .command("lead-time")
+    .description("Delivery Lead Time is inspired by the Accelerate book by Dr. Forsgren, " +
+      "it finds when a release was made and compares it to the oldest commit in that release, " +
+      "which speaks to how long it took from the code was committed until that code was released" +
+      "\n(this assumes a GitHub release equals running in production)")
     .argument("<token>", "GitHub Personal Access Token")
     .argument("<path>", "Repository path in the form of <owner>/<repo>, e.g. \"gaggle/github-delivery-metrics\"")
     .action((token, path, opts, command: Command) => {
       command.setOptionValue("token", token);
       command.setOptionValue("path", path);
     })
-    .option("-d, --debug", "Enable debug logging");
+
   program.parse(argv);
   return program.opts();
 }
